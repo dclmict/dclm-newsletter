6e7e81b045a7be4ecca0f38dcf2451b67db79388
Samuel John <opeoniye@gmail.com>
2022-11-27 18:05:41 +0100

    converting app into containers and microservices

---
+++ up.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# ____          __  __  _____ 
+# |  _ \   /\   |  \/  |/ ____|     https://github.com/opeoniye
+# | |_) | /  \  | \  / | (___       https://opeoniye.vercel.app/
+# |  _ < / /\ \ | |\/| |\___ \      credit: http://patorjk.com/software/taag/
+# | |_) / ____ \| |  | |____) |
+# |____/_/    \_\_|  |_|_____/ 
+#                             
+#
+# Based on https://gist.github.com/2206527
+
+# Be pretty
+echo -e " "
+echo -e "  ______________________________"
+echo -e " |                              |"
+echo -e "  | DCLM Newsletter Standup script v.1.0 |"
+echo -e " /       © DCLM 2022 |"
+echo -e " |______________________________|"
+echo -e " "
+
